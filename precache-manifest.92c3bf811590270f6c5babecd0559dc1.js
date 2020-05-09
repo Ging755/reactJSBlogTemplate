a4f8bb8b5911196c21da6815a61ae4f4b3fe1e63
Ging755 <denis.milic2@gmail.com>
2020-05-09 19:43:24 +0200

    Updates

---
+++ precache-manifest.92c3bf811590270f6c5babecd0559dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a9c24009991d82730fd567a1e55e75",
+    "url": "/reactJSBlogTemplate/index.html"
+  },
+  {
+    "revision": "b7d4e29d31c8ccb42dc5",
+    "url": "/reactJSBlogTemplate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bc21dab2472512c2076",
+    "url": "/reactJSBlogTemplate/static/js/2.a38d4cf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactJSBlogTemplate/static/js/2.a38d4cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d4e29d31c8ccb42dc5",
+    "url": "/reactJSBlogTemplate/static/js/main.f135f403.chunk.js"
+  },
+  {
+    "revision": "8e84ee34dc90e6f6a162",
+    "url": "/reactJSBlogTemplate/static/js/runtime-main.be567e5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactJSBlogTemplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
